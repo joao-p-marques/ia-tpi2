3e3ec7ab9c8ecafa712ea4829b92279163a1f784
Luis Seabra Lopes <lsl@ua.pt>
2019-11-29 21:00:02 +0000

    TPI-2

---
+++ tpi2/tpi2.py
@@ -0,0 +1,26 @@
+
+
+from semantic_network import *
+from bayes_net import *
+
+
+class MySN(SemanticNetwork):
+
+
+    def query_dependents(self,entity):
+        #IMPLEMENT HERE
+
+    def query_causes(self, entity):
+        #IMPLEMENT HERE
+
+    def query_causes_sorted(self,entity):
+        #IMPLEMENT HERE
+
+
+class MyBN(BayesNet):
+
+    def markov_blanket(self,var):
+        #IMPLEMENT HERE
+        pass
+
+
